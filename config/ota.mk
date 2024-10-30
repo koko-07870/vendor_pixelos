f668d8c0009a93db11e7e1f246cae36adf6687e1
Hridaya Prajapati <hridaya@pixelos.net>
2024-10-30 13:58:51 +0100

    vendor: Updates for our custom Updater
    Change-Id: I2248f79906dc1f2d61499a34b7c4e6b82cbf607e

--- config/ota.mk
+++ config/ota.mk
@@ -1,8 +1,5 @@
 # Updater
 ifeq ($(IS_OFFICIAL),true)
     PRODUCT_PACKAGES += \
-        Updates
-
-    PRODUCT_COPY_FILES += \
-        vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
+        Updater
 endif
